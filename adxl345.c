54fa80025bcda7709e42a4ca1ab41e623ee3daed
David O'Mahony <david.c.omahony@gmail.com>
2015-09-11 19:19:54 -0230

    Updates to support ADXL345

--- adxl345.c
+++ adxl345.c
@@ -4,16 +4,22 @@
  *  Created on: Sep 10, 2015
  *      Author: domahony
  */
+#include "i2c.h"
 #include "adxl345.h"
+
+static uint8_t devid;
 
 void
 adxl345_init()
 {
-
+	i2c_begin(0x53);
+	devid = i2c_read8(0);
+	i2c_end();
 }
 
 int
 adxl345_get_reading(GY80* gy80)
 {
+	gy80->accelerometer.devid = devid;
 	return 1;
 }
